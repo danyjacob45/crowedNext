c6fe81698eb6862018424d133e774a79c325450e
unknown <badagadzeb@gmail.com>
2021-03-18 21:52:21 +0400

    finan

--- pages/financial.tsx
+++ pages/financial.tsx
@@ -14,6 +14,7 @@ const financial = () => {
   const [transactions, setTransactions] = useState([]);
   const [team, setTeam] = useState<any>([]);
   const [directComissions, setDirectComissions] = useState<any>([]);
+  const [investments, setInvestments] = useState<any>([]);
 
   useEffect(() => {
     AuthService.transactions().then((res) => {
@@ -26,15 +27,54 @@ const financial = () => {
       let teams: any = [];
 
       for (const key in res.data.referrers) {
-        teams.push(res.data.referrers[key]);
+        res.data.referrers[key].map((el) => {
+          teams.push(el);
+        });
       }
+      console.log(teams);
       // debugger;
       // debugger;
       setTeam(teams);
       setDirectComissions(res.data.directComissions);
       // debugger;
     });
+    AuthService.profits()
+      .then((res) => {
+        setInvestments(res.data.profits);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
+
+  const getSum = (arr, key) => {
+    let acumulator = 0;
+
+    arr.map((el) => {
+      if (el[key]) {
+        acumulator += el[key];
+      }
+    });
+
+    return acumulator;
+  };
+
+  const getLastDate = (date: any) => {
+    let time = new Date(date);
+    return `${time.getFullYear()}/${
+      time.getMonth() + 1
+    }/${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+    //   "/" +
+    //   (time.getMonth() + 1) +
+    //   "/" +
+    //   time.getDate() +
+    //   " "}
+    // {time.getHours() +
+    //   ":" +
+    //   time.getMinutes() +
+    //   ":" +
+    //   time.getSeconds()}
+  };
 
   return (
     <Layout title="financial">
@@ -214,7 +254,12 @@ const financial = () => {
                       $
                     </th>
                     <th className="bold12">ETH</th>
-                    <th className="bold12">15/3/2021 12:12</th>
+                    <th className="bold12">
+                      {transactions.length &&
+                        // @ts-ignore: Unreachable code error
+
+                        getLastDate(transactions[0].createdAt)}
+                    </th>
                   </tr>
                 </thead>
 
@@ -251,6 +296,93 @@ const financial = () => {
                   })}
                 </tbody>
                 {/* /////////// */}
+
+                <thead
+                  onClick={() => {
+                    if (activeTab === "ProfitShare") {
+                      setActiveTab(null);
+                    } else {
+                      setActiveTab("ProfitShare");
+                    }
+                  }}
+                >
+                  <tr>
+                    <th>
+                      <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="plus-circle"
+                        role="img"
+                        width="11"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        className="svg-inline--fa fa-plus-circle fa-w-16 fa-2x"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"
+                          className=""
+                        ></path>
+                      </svg>
+                    </th>
+                    <th className="bold12">Profit Share</th>
+                    <th className="bold12">
+                      {/* {investments.length &&
+                        // @ts-ignore: Unreachable code error
+                        transactions?.reduce((sum: any, el: any) => {
+                          if (sum.finalAmount) {
+                            return sum.finalAmount + el.finalAmount;
+                          }
+                          return sum + el.finalAmount;
+                        })}{" "} */}
+                      {investments.length && getSum(investments, "amount")}$
+                    </th>
+                    <th className="bold12">
+                      Earned $
+                      {investments.length &&
+                        getSum(investments, "profit").toFixed(2)}
+                    </th>
+                    <th className="bold12">
+                      {investments.length &&
+                        getLastDate(investments[0].createdAt)}
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody
+                  style={{
+                    display: activeTab === "ProfitShare" ? "contents" : "none",
+                  }}
+                >
+                  {investments.map((el: any, i) => {
+                    const time = new Date(el.createdAt);
+                    return (
+                      <tr key={i}>
+                        <td></td>
+
+                        <td>{"Profit Share"} </td>
+                        <td>{el.amount} $</td>
+
+                        <td> ${el.profit}</td>
+                        <td>
+                          {" "}
+                          {time.getFullYear() +
+                            "/" +
+                            (time.getMonth() + 1) +
+                            "/" +
+                            time.getDate() +
+                            " "}
+                          {time.getHours() +
+                            ":" +
+                            time.getMinutes() +
+                            ":" +
+                            time.getSeconds()}{" "}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
                 {/* //////////////////////////// */}
                 <thead
                   onClick={() => {
@@ -308,7 +440,12 @@ const financial = () => {
                           })
                           ?.toFixed(2)}
                     </th>
-                    <th className="bold12">15/3/2021 12:12</th>
+                    <th className="bold12">
+                      {directComissions.length &&
+                        // @ts-ignore: Unreachable code error
+
+                        getLastDate(directComissions[0].createdAt)}
+                    </th>
                   </tr>
                 </thead>
 
@@ -344,7 +481,183 @@ const financial = () => {
                     );
                   })}
                 </tbody>
+
+                {/* ////// */}
+
+                <thead
+                  onClick={() => {
+                    if (activeTab === "ResidualBonus") {
+                      setActiveTab(null);
+                    } else {
+                      setActiveTab("ResidualBonus");
+                    }
+                  }}
+                >
+                  <tr>
+                    <th>
+                      <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="plus-circle"
+                        role="img"
+                        width="11"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        className="svg-inline--fa fa-plus-circle fa-w-16 fa-2x"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"
+                          className=""
+                        ></path>
+                      </svg>
+                    </th>
+                    <th className="bold12">Residual Bonus</th>
+                    <th className="bold12">
+                      {/* {investments.length &&
+                        // @ts-ignore: Unreachable code error
+                        transactions?.reduce((sum: any, el: any) => {
+                          if (sum.finalAmount) {
+                            return sum.finalAmount + el.finalAmount;
+                          }
+                          return sum + el.finalAmount;
+                        })}{" "} */}
+                      {team.length && getSum(team, "investedAmount")}$
+                    </th>
+                    <th className="bold12">
+                      Earned ${team.length && getSum(team, "earned").toFixed(2)}
+                    </th>
+                    <th className="bold12">
+                      {team.length && getLastDate(team[0].createdAt)}
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody
+                  style={{
+                    display:
+                      activeTab === "ResidualBonus" ? "contents" : "none",
+                  }}
+                >
+                  {team.map((el: any, i) => {
+                    const time = new Date(el.createdAt);
+                    return (
+                      <tr key={i}>
+                        <td></td>
+
+                        <td>{"Profit Share"} </td>
+                        <td>{el.investedAmount} $</td>
+
+                        <td> ${el.earned}</td>
+                        <td>
+                          {" "}
+                          {time.getFullYear() +
+                            "/" +
+                            (time.getMonth() + 1) +
+                            "/" +
+                            time.getDate() +
+                            " "}
+                          {time.getHours() +
+                            ":" +
+                            time.getMinutes() +
+                            ":" +
+                            time.getSeconds()}{" "}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+
+                <thead
+                  onClick={() => {
+                    if (activeTab === "Investment") {
+                      setActiveTab(null);
+                    } else {
+                      setActiveTab("Investment");
+                    }
+                  }}
+                >
+                  <tr>
+                    <th>
+                      <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="plus-circle"
+                        role="img"
+                        width="11"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        className="svg-inline--fa fa-plus-circle fa-w-16 fa-2x"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"
+                          className=""
+                        ></path>
+                      </svg>
+                    </th>
+                    <th className="bold12">Investment</th>
+                    <th className="bold12">
+                      {/* {investments.length &&
+                        // @ts-ignore: Unreachable code error
+                        transactions?.reduce((sum: any, el: any) => {
+                          if (sum.finalAmount) {
+                            return sum.finalAmount + el.finalAmount;
+                          }
+                          return sum + el.finalAmount;
+                        })}{" "} */}
+                      {investments.length && getSum(investments, "amount")}$
+                    </th>
+                    <th className="bold12">
+                      Earned $
+                      {investments.length &&
+                        getSum(investments, "profit").toFixed(2)}
+                    </th>
+                    <th className="bold12">
+                      {investments.length &&
+                        getLastDate(investments[0].createdAt)}
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody
+                  style={{
+                    display: activeTab === "Investment" ? "contents" : "none",
+                  }}
+                >
+                  {investments.map((el: any, i) => {
+                    const time = new Date(el.createdAt);
+                    return (
+                      <tr key={i}>
+                        <td></td>
+
+                        <td>{"Investment"} </td>
+                        <td>{el.amount} $</td>
+
+                        <td> ${el.profit}</td>
+                        <td>
+                          {" "}
+                          {time.getFullYear() +
+                            "/" +
+                            (time.getMonth() + 1) +
+                            "/" +
+                            time.getDate() +
+                            " "}
+                          {time.getHours() +
+                            ":" +
+                            time.getMinutes() +
+                            ":" +
+                            time.getSeconds()}{" "}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
               </table>
+
+              {/* /\//// */}
             </div>
           </div>
         </div>
