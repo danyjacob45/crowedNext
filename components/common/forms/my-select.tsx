3f27f31f81834533e0b36c0e51a0f3f2f0b83acc
unknown <badagadzeb@gmail.com>
2021-04-03 23:39:21 +0400

    nth

--- components/common/forms/my-select.tsx
+++ components/common/forms/my-select.tsx
@@ -7,44 +7,10 @@ interface IOption {
   label: string;
 }
 
-type Props = {
-  hasError: boolean;
-  options: IOption[];
-  label?: string;
-  defaultValue?: string;
-  onChange: Function;
-  errorMessage?: string;
-  errors?: string[];
-  value?: string;
-  placeholder?: string;
-};
+type Props = {};
 
-const MySelect = ({
-  hasError,
-  defaultValue,
-  onChange,
-  errorMessage,
-  errors,
-  options,
-  placeholder,
-  value,
-}: Props) => {
-  return (
-    <>
-      <div>
-        <ReactSelect
-          placeholder={placeholder}
-          className={classNames({ "border border-danger rounded": hasError })}
-          defaultValue={options.filter((i) => i.value === defaultValue)}
-          value={value ? options.filter((i) => i.value === value) : undefined}
-          onChange={onChange as any}
-          options={options}
-        />
-        {errorMessage && <span className="text-danger">{errorMessage}</span>}
-        {errors && errors.map((error) => <div>{error}</div>)}
-      </div>
-    </>
-  );
+const MySelect = ({}: Props) => {
+  return <></>;
 };
 
 export { MySelect };
